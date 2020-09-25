4f52d70750682357961d58d75c01d10c7fd7009b
Hillel Saal <remi.saal@gmail.com>
2020-09-25 13:24:27 +0200

    Extracted Illustration

---
+++ src/pages/Home/Illustration.tsx
@@ -0,0 +1,151 @@
+import React from "react"
+
+import styles from "./Home.scss"
+
+const Illustration = () => (
+  <svg className={styles.background} viewBox="0 0 484.74 798.13">
+    <polygon
+      style={{ fill: "#d7f5ff" }}
+      points="483.74 0 483.74 798.13 63.74 609.69 63.74 0 483.74 0"
+    />
+    <polygon
+      style={{ fill: "#bdefff" }}
+      points="484.74 0 143.74 0 143.74 562.27 484.74 714.61 484.74 0"
+    />
+    <g className={styles.urn}>
+      <path
+        style={{ fill: "#008ebb" }}
+        d="M195.66,738,.58,640.43v-240L195.66,497.9Z"
+      />
+      <path
+        style={{ fill: "#00b6ef" }}
+        d="M195.66,738l195.08-97.53V400.36L195.66,497.89Z"
+      />
+      <path
+        style={{ fill: "#20c9ff" }}
+        d="M0,400.47,187.45,303l202.39,97.47L194.89,497.88Z"
+      />
+      <rect
+        style={{ fill: "#fff" }}
+        x="99.1"
+        y="246.58"
+        width="135.49"
+        height="183.66"
+        rx="8.44"
+        transform="translate(-134.07 110.79) rotate(-26.65)"
+      />
+      <rect
+        style={{ fill: "#dcdcdc" }}
+        x="150.42"
+        y="381.75"
+        width="70"
+        height="7.52"
+        rx="3.76"
+        transform="translate(-153.22 124.13) rotate(-26.65)"
+      />
+      <rect
+        style={{ fill: "#afafaf" }}
+        x="131.48"
+        y="374.29"
+        width="62.09"
+        height="7.52"
+        rx="3.76"
+        transform="translate(-152.31 113.06) rotate(-26.65)"
+      />
+      <rect
+        style={{ fill: "#dcdcdc" }}
+        x="156.1"
+        y="394.38"
+        width="58.72"
+        height="7.52"
+        rx="3.76"
+        transform="translate(-158.88 125.48) rotate(-26.65)"
+      />
+      <path
+        style={{ fill: "#dcdcdc" }}
+        d="M166,416.34l49.77-25a3.76,3.76,0,0,1,5.06,1.67h0a3.76,3.76,0,0,1-1.68,5.05l-49.78,25a3.76,3.76,0,0,1-5-1.67h0A3.75,3.75,0,0,1,166,416.34Z"
+      />
+      <path
+        style={{ fill: "#dcdcdc" }}
+        d="M132.35,349.41l55.84-28a3.75,3.75,0,0,1,5.05,1.67h0a3.76,3.76,0,0,1-1.68,5.05l-55.83,28a3.77,3.77,0,0,1-5.06-1.67h0A3.76,3.76,0,0,1,132.35,349.41Z"
+      />
+      <path
+        style={{ fill: "#afafaf" }}
+        d="M107.92,330.09l87.45-43.89a3.77,3.77,0,0,1,5,1.67h0a3.75,3.75,0,0,1-1.68,5.05L111.3,336.81a3.78,3.78,0,0,1-5.06-1.67h0A3.76,3.76,0,0,1,107.92,330.09Z"
+      />
+      <path
+        style={{ fill: "#878787" }}
+        d="M89.53,293.42,177,249.54a3.75,3.75,0,0,1,5.05,1.67h0a3.76,3.76,0,0,1-1.68,5L92.9,300.14a3.75,3.75,0,0,1-5.05-1.67h0A3.76,3.76,0,0,1,89.53,293.42Z"
+      />
+      <path
+        style={{ fill: "#878787" }}
+        d="M94.59,303.51,182,259.62a3.78,3.78,0,0,1,5.06,1.68h0a3.77,3.77,0,0,1-1.68,5.05L98,310.23a3.75,3.75,0,0,1-5-1.67h0A3.75,3.75,0,0,1,94.59,303.51Z"
+      />
+      <path
+        style={{ fill: "#878787" }}
+        d="M99.66,313.6l40.69-20.42a3.77,3.77,0,0,1,5.06,1.67h0a3.76,3.76,0,0,1-1.68,5L103,320.33a3.76,3.76,0,0,1-5-1.68h0A3.77,3.77,0,0,1,99.66,313.6Z"
+      />
+      <path
+        style={{ fill: "#afafaf" }}
+        d="M113,340.18,141.23,326a3.76,3.76,0,0,1,5.05,1.68h0a3.76,3.76,0,0,1-1.68,5.05L116.36,346.9a3.76,3.76,0,0,1-5.06-1.67h0A3.76,3.76,0,0,1,113,340.18Z"
+      />
+      <path
+        style={{ fill: "#dcdcdc" }}
+        d="M137.43,359.5l45.74-23a3.76,3.76,0,0,1,5.06,1.67h0a3.76,3.76,0,0,1-1.68,5.05l-45.75,23a3.77,3.77,0,0,1-5.05-1.67h0A3.76,3.76,0,0,1,137.43,359.5Z"
+      />
+      <circle style={{ fill: "#dcdcdc" }} cx="122.28" cy="358.67" r="3.77" />
+      <circle style={{ fill: "#dcdcdc" }} cx="127.35" cy="368.76" r="3.77" />
+      <circle style={{ fill: "#02aee4" }} cx="122.28" cy="358.67" r="1.88" />
+      <rect
+        style={{ fill: "#dcdcdc" }}
+        x="141.96"
+        y="401.68"
+        width="7.52"
+        height="7.52"
+        transform="translate(-166.38 108.44) rotate(-26.65)"
+      />
+      <rect
+        style={{ fill: "#dcdcdc" }}
+        x="147.03"
+        y="411.77"
+        width="7.52"
+        height="7.52"
+        transform="translate(-170.36 111.78) rotate(-26.65)"
+      />
+      <rect
+        style={{ fill: "#dcdcdc" }}
+        x="152.09"
+        y="421.86"
+        width="7.52"
+        height="7.52"
+        transform="translate(-174.35 115.12) rotate(-26.65)"
+      />
+      <path
+        style={{ fill: "none", stroke: "#02aee4", strokeWidth: "3.06px" }}
+        d="M143.17,406.25l2.67.79,1.71-4.65"
+      />
+      <path
+        style={{ fill: "none", stroke: "#02aee4", strokeWidth: "3.06px" }}
+        d="M153.51,426.76l2.67.79,1.71-4.62"
+      />
+      <rect
+        style={{ fill: "#04c3ff", stroke: "#000", strokeWidth: "6.06px" }}
+        x="129.88"
+        y="405.04"
+        width="152.79"
+        height="20.1"
+        transform="translate(-163.98 136.23) rotate(-26.59)"
+      />
+      <rect
+        style={{ fill: "#20c9ff" }}
+        x="125.12"
+        y="405.28"
+        width="165.28"
+        height="25.22"
+        transform="translate(-165.07 137.2) rotate(-26.59)"
+      />
+    </g>
+  </svg>
+)
+
+export default Illustration
